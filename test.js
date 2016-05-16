e004694c9245b6a19102e10086aa4b957c21f7c9
Matheus <matheus.frndes@gmail.com>
2016-05-16 00:52:53 -0300

    Catch promise rejection in tests

--- test.js
+++ test.js
@@ -6,20 +6,29 @@ import test from 'ava';
 import translate from './index';
 
 test('translate from \'\' (auto) to \'\' (english)', async t => {
-    const res = await translate('Ik spreek Engels', {});
-    t.falsy(res.text.corrected);
-    t.falsy(res.text.correction);
-    t.is(res.text.value, 'I speak English');
-    t.falsy(res.from.corrected);
-    t.is(res.from.iso, 'nl');
+    try {
+        const res = await translate('Ik spreek Engels');
+
+        t.falsy(res.text.corrected);
+        t.falsy(res.text.correction);
+        t.is(res.text.value, 'I speak English');
+        t.falsy(res.from.corrected);
+        t.is(res.from.iso, 'nl');
+    } catch (err) {
+        t.fail(err.code);
+    }
 });
 
 test('translate from auto to dutch', async t => {
-    const res = await translate('I speak Dutch', {from: 'auto', to: 'nl'});
+    try {
+        const res = await translate('I speak Dutch', {from: 'auto', to: 'nl'});
 
-    t.falsy(res.text.corrected);
-    t.falsy(res.text.correction);
-    t.is(res.text.value, 'ik spreek Nederlands');
-    t.falsy(res.from.corrected);
-    t.is(res.from.iso, 'en');
+        t.falsy(res.text.corrected);
+        t.falsy(res.text.correction);
+        t.is(res.text.value, 'ik spreek Nederlands');
+        t.falsy(res.from.corrected);
+        t.is(res.from.iso, 'en');
+    } catch (err) {
+        t.fail(err.code);
+    }
 });
