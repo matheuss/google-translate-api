4db0d74db2cc65a3303eea4edc42b86406737735
Bryan Ellerbrock <you@example.com>
2018-11-29 14:09:00 -0500

    change client to gtx

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function translate(text, opts) {
     return token.get(text).then(function (token) {
         var url = 'https://translate.google.com/translate_a/single';
         var data = {
-            client: 't',
+            client: 'gtx',
             sl: opts.from,
             tl: opts.to,
             hl: opts.to,
