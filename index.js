1bfad3b59f3bcd80d2f28983d24292c8fae61a99
Colton G. Rushton <colton51919@gmail.com>
2019-03-12 09:07:49 -0300

    Update index.js
    Reverts previous commit as I'm unsure how this is supposed to work

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function translate(text, opts) {
     opts.to = languages.getCode(opts.to);
 
     return token.get(text).then(function (token) {
-        var tld = opts.tld = opts.tls || 'com';
+        opts.tld = opts.tls || 'com';
         var url = 'https://translate.google.' + opts.tld + '/translate_a/single';
         var data = {
             client: opts.client || 't',
