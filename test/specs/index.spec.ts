d70c32253c21e0b50257bf233e5a5d194b6d1309
Vitaliy Potapov <noginsk@rambler.ru>
2023-05-12 13:35:13 +0400

    test: translate from/to en and between non-en

--- test/specs/index.spec.ts
+++ test/specs/index.spec.ts
@@ -2,13 +2,33 @@ import fs from 'fs';
 import createHttpProxyAgent from 'http-proxy-agent';
 import { extractTooManyRequestsInfo } from '../../src/helpers.js';
 
-it('translate', async () => {
+it('translate to en', async () => {
   const { text, raw } = await translate('Привет, мир! Как дела?', { to: 'en' });
   assert.equal(text, 'Hello World! How are you?');
   assert.equal(raw.src, 'ru');
   assert.equal(raw.sentences.length, 3);
   assert.deepEqual(raw.sentences.slice(-1), [
     { src_translit: 'Privet, mir! Kak dela?' }
+  ]);
+});
+
+it('translate from en', async () => {
+  const { text, raw } = await translate('Hello! How are you?', { to: 'zh' });
+  assert.equal(text, '你好！你好吗？');
+  assert.equal(raw.src, 'en');
+  assert.equal(raw.sentences.length, 3);
+  assert.deepEqual(raw.sentences.slice(-1), [
+    { translit: 'Nǐ hǎo! Nǐ hǎo ma?' }
+  ]);
+});
+
+it('translate between non-en', async () => {
+  const { text, raw } = await translate('你好！你好吗？', { to: 'ru' });
+  assert.equal(text, 'Привет! Ты в порядке?');
+  assert.equal(raw.src, 'zh-CN');
+  assert.equal(raw.sentences.length, 3);
+  assert.deepEqual(raw.sentences.slice(-1), [
+    { src_translit: 'Nǐ hǎo! Nǐ hǎo ma?', translit: 'Privet! Ty v poryadke?' }
   ]);
 });
 
